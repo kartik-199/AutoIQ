7a2bb941d86a2bcb0163800255d8a8995ea2d016
kartik-199 <kathuriakartik2005@gmail.com>
2024-12-18 13:55:13 -0500

    closing dev changes

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from models.access_model import get_model_prediction
-
+import json
 from flask import Flask, render_template, request
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -14,6 +15,7 @@ def predict_page():
 
 @app.route('/predict', methods = ['POST'])
 def predict():
+    # Get prediction
     displacement = float(request.form['displacement'])
     cylinders = float(request.form['cylinders'])
     horsepower = float(request.form['cylinders'])
@@ -23,14 +25,46 @@ def predict():
     origin = int(request.form['origin'])
     prediction = get_model_prediction(weight, acceleration, displacement, cylinders, horsepower, model_year, origin)
     prediction = f"{prediction: .2f}"
-    return render_template('predict.html', prediction=prediction, displacement=displacement,
-                           cylinders=cylinders, horsepower=horsepower, weight=weight, acceleration=acceleration,
-                           model_year=model_year, origin=origin)
+
+    # Acces and modify leaderboard data if necessary
+    new_entry = {
+        "displacement": displacement,
+        "cylinders": cylinders,
+        "mpg": prediction,
+        "date": datetime.now().strftime("%m-%d-%y %H:%M")
+    }
+    try:
+        with open("leaderboard.json", "r") as file:
+            leaderboard_data = json.load(file)
+    except (FileNotFoundError, json.JSONDecodeError):
+        leaderboard_data = []
+    leaderboard_data.append(new_entry)
+    leaderboard_data = sorted(leaderboard_data, key=lambda x: x["mpg"], reverse=True)  # Sort by MPG (descending)
+    leaderboard_data = leaderboard_data[:10]
+    with open("leaderboard.json", "w") as file:
+        json.dump(leaderboard_data, file, indent=4)
+    # Return predict template
+    return render_template(
+        'predict.html',
+        prediction=prediction,
+        displacement=displacement,
+        cylinders=cylinders,
+        horsepower=horsepower,
+        weight=weight,
+        acceleration=acceleration,
+        model_year=model_year,
+        origin=origin
+    )
 
 
 @app.route('/leaderboard')
 def leaderboard_page():
-    return render_template('leaderboard.html')
+    try:
+        with open("leaderboard.json", "r") as file:
+            leaderboard_data = json.load(file)
+    except (FileNotFoundError, json.JSONDecodeError):
+        leaderboard_data = []
+    return render_template('leaderboard.html', leaderboard=leaderboard_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
